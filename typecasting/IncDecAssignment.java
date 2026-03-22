5758342a280c42e7b7709d08c68bc24ec5853e6e
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-03-22 11:18:50 +0545

    code updated upto opereators

---
+++ typecasting/IncDecAssignment.java
@@ -0,0 +1,102 @@
+package typecasting;
+
+public class IncDecAssignment {
+public static void main(String[] args) {
+	/*
+	 * Q1.
+	 * -----------------------------------------------------------------------
+	 * 	int x = 3;
+		int res = x++ + ++x + x++ + ++x + x-- + --x + x++ + x-- + ++x;
+		System.out.println(res);
+		System.out.println(x);
+	 * ------------------------------------------------------------------------
+	 * 
+	 *  * Q2.
+	 * -----------------------------------------------------------------------
+	 * 	 int a = 5;
+        	 int b = 3;
+        	 System.out.println(a++ + ++b - --a);     
+         System.out.println(a-- + b++ + ++a);     
+         System.out.println(++a + b-- - a++);     
+         System.out.println(a + b);               
+         System.out.println(--b + --a + b++);     
+         System.out.println(a++ - ++b + a--);     
+         System.out.println(a);                   
+         System.out.println(b);     
+	 * ------------------------------------------------------------------------
+	 * 
+	 *  Q3.
+	 * -----------------------------------------------------------------------
+	 * 	   int x = 7;
+        	   int y = 4;
+        	   x = x++ + ++x - x--;
+           y = --y + y++ + ++y;
+           System.out.println(x + y);               
+           System.out.println(x++ + --y - ++x);     
+           x += y-- + ++x;
+           System.out.println(x);                   
+           System.out.println(y);                   
+           System.out.println(--x + y++ - x--);     
+	 * ------------------------------------------------------------------------
+	 * 
+	 *  Q4.
+	 * -----------------------------------------------------------------------
+	 * 	   int x = 7;
+        	   int y = 4;
+        	   x = x++ + ++x - x--;
+           y = --y + y++ + ++y;
+           System.out.println(x + y);               
+           System.out.println(x++ + --y - ++x);     
+           x += y-- + ++x;
+           System.out.println(x);                   
+           System.out.println(y);                   
+           System.out.println(--x + y++ - x--);     
+	 * ------------------------------------------------------------------------
+	 * 
+	 *  Q5.
+	 * -----------------------------------------------------------------------
+	 * 	    
+	 	int x = 3;
+        System.out.println(x++ + ++x + x--);     
+        System.out.println(--x + x++ - ++x);     
+        x = x++ + x++ + ++x;
+        System.out.println(x);                   
+        System.out.println(x-- - --x + x++);     
+        x += ++x - x-- + x++;
+        System.out.println(x);                   
+        System.out.println(--x);    
+	 * ------------------------------------------------------------------------
+	 * 
+	 * Q6. 
+	 * -------------------------------------------------------------------------
+	 *   int a = 2;
+        	 int b = 5;
+
+        System.out.println(a++ + ++b - a-- + --b);   
+        a = a++ + ++a + b--;
+        System.out.println(a + b);                   
+        b = --b + a++ - ++a + b++;
+        System.out.println(a-- + b++ + --a);         
+        System.out.println(++a + --b + a++);         
+        System.out.println(a);                       
+        System.out.println(b); 
+        ------------------------------------------------------------------------
+        Q7. 
+        ------------------------------------------------------------------------
+        int a = 4;
+		int b = 6;
+		int c = 2;
+		
+		System.out.println(a++ + --b + c++);
+		System.out.println(++a - b-- + ++c);
+		c = a++ + b++ - --c;
+		System.out.println(a + b + c);
+		a = --a + ++b - c--;
+		System.out.println(a++ + b-- + --c);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	 */
+	
+}
+}
