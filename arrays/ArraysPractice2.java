ecd18a1cc5130f5c29a40035080cdefe0a3cf9ba
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-07 19:58:34 +0545

    upddated

--- arrays/ArraysPractice2.java
+++ arrays/ArraysPractice2.java
@@ -4,64 +4,46 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArraysPractice2 {
-
-    static double[] marks = new double[5];
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        inputMarks(sc);
-        printMarks();
-
-        double total = calculateTotal();
-        double percentage = calculatePercentage(total);
-        boolean result = isPass();
-
-        System.out.println("Total marks = " + total);
-        System.out.println("Percentage = " + percentage + "%");
-        System.out.println(result ? "You are Pass!" : "You are Fail!");
-
-        sc.close();
-    }
-
-    static void inputMarks(Scanner sc) {
-        for (int i = 0; i < marks.length; i++) {
-            while (true) {
-                System.out.print("Enter marks " + (i + 1) + ": ");
-                double enteredMarks = sc.nextDouble();
-
-                if (enteredMarks >= 0 && enteredMarks <= 100) {
-                    marks[i] = enteredMarks;
-                    break;
-                } else {
-                    System.out.println("Please enter valid marks (0-100).");
-                }
-            }
-        }
-    }
-
-    static void printMarks() {
-        System.out.println("Your marks: " + Arrays.toString(marks));
-    }
-
-    static double calculateTotal() {
-        double sum = 0;
-        for (double mark : marks) {
-            sum += mark;
-        }
-        return sum;
-    }
-
-    static double calculatePercentage(double total) {
-        return (total / 500) * 100;
-    }
-
-    static boolean isPass() {
-        for (double mark : marks) {
-            if (mark < 40) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
+	/*
+	 * Q. 5 marks ---- student -pass fail total percentage: ----
+	 *  
+	 */
+	
+	static double[] marks = new double[5];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//1. assign values:
+		for(int i=0; i<marks.length; i++) 
+			while(true) {
+				System.out.println("Enter marks "+(i+1)+": ");
+				double enteredMarks = sc.nextDouble();
+				
+				if(enteredMarks>=0 && enteredMarks<=100) {
+					marks[i] = enteredMarks;
+					break;
+				}else {
+					System.out.println("Please enter valid marks (1-100): ");
+				}
+			}
+			
+		
+		// print marks:
+		System.out.println("Your marks: "+ Arrays.toString(marks));
+		double sum = 0;
+		for(int i=0; i<marks.length; i++) {
+			if(marks[i] >=40) {
+				sum+=marks[i];
+		}else {
+			System.out.println("You are failed!");
+			break;
+		}
+		
+	}
+		
+		if(sum>=200) {
+			System.out.println("You are pass!");
+			double percent = (sum/500)*100;
+			System.out.println("Your percentage is: "+percent+"%");
+		}
+	
+}}
