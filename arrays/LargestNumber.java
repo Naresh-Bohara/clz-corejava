bc1a50f65e441c5f28099352b013c95c0b91eace
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-08 09:12:32 +0545

    updated upto array

--- arrays/LargestNumber.java
+++ arrays/LargestNumber.java
@@ -1,7 +1,28 @@
 package arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class LargestNumber {
 	public static void main(String[] args) {
+		int[] arr = new int[4];
 		
+		Scanner sc = new Scanner(System.in);
+		
+		// assign value to array:
+		for(int i=0; i<arr.length; i++) {
+			System.out.println("Enter number "+(i+1)+": ");
+			arr[i]= sc.nextInt();
+		}
+		 
+		int max = arr[0];
+	
+		for(int i=0; i< arr.length; i++) {
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+		System.out.println("Largest Number is: "+ max);
 	}
 }
