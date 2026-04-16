ed9df2d8424b98de66143ffb9afbfc8b0632f444
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-16 09:13:20 +0545

    added assignment

---
+++ arraywithmethods/assignment/AssignmentDocs.java
@@ -0,0 +1,34 @@
+package arraywithmethods.assignment;
+
+public class AssignmentDocs {
+/*
+ * Q1. Write a Java program to create a method that takes an array of product prices as an argument and returns a new array after applying a 10% discount to each product.
+
+Requirements:
+- Use a method to take array input from user.
+- Pass the array to another method.
+- Return a new array with updated prices.
+- Display the final discounted prices.
+
+
+Q2. Write a Java program to create a method that takes an array of student marks as an argument and returns a new array after adding 5 grace marks to each student. Ensure that marks do not exceed 100.
+
+Requirements:
+- Input marks using a method.
+- Pass the array as parameter.
+- Return updated marks array.
+- Print the final result.
+
+
+Q3. Write a Java program to create a method that takes an array of product stock quantities as an argument and returns a new array after updating stock as follows:
+- If stock is less than 10, update it to 50.
+- Otherwise, keep it unchanged.
+
+Requirements:
+- Use separate methods for input and processing.
+- Pass array as argument.
+- Return updated array.
+- Display the result.
+
+ */
+}
